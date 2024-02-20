dd389ab1d56a1cf02567cad43d73b8e8b595877f
YgorVSPaulino <ygor.paulino.1999@gmail.com>
2024-02-20 10:38:59 -0300

    aprendendo react

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-
+import React from 'react';
+import Main from './components/Main';
 import './App.css';
 
 export default function App() {
-  return (
-    <div className="App">
-       <h1>Hello World!</h1>
-    </div>
-  );
+  return <Main />;
 }
 
 
